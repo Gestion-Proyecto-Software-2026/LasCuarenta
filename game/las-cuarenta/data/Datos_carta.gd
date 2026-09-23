40415c66c95a4081b933692a756deb6571261c05
Portatil <869688@unizar.es>
2026-09-23 19:55:06 +0200

    add: carta
    añadido el objeto y esscena carta godot

--- game/las-cuarenta/data/Datos_carta.gd
+++ game/las-cuarenta/data/Datos_carta.gd
@@ -2,11 +2,9 @@ extends Resource
 
 class_name Carta
 
-enum palos{
-	oros, copas, espadas, bastos
-}
+
 
 @export var id : String = "reverse"
-@export var palo : palos= palos.oros
+@export var palo :  FuncionesGlobales.palos= FuncionesGlobales.palos.oros
 @export_range(0,12,1) var valor := 0
 @export var imagen : CompressedTexture2D 
